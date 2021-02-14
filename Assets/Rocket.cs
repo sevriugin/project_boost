4800ea34379b4a8a4b2506693f8f8f8108d49f12
Sergei Sevriugin <sevriugin@gmail.com>
2021-02-14 08:57:43 +0100

    add sound to rocket

---
+++ Assets/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       ProcessInput(); 
+    }
+
+    void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space)) {
+            print("thrusting ");
+            rigidBody.AddRelativeForce(Vector3.up);
+        } 
+        
+        if (Input.GetKey(KeyCode.A)) {
+            print("rotaiting left ");
+            transform.Rotate(Vector3.forward);
+        } else if (Input.GetKey(KeyCode.D)) {
+            print("rotaiting right ");
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
