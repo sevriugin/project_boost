43be47aca6b41be1facee3601f2c63b50fe948df
Sergei Sevriugin <sevriugin@gmail.com>
2021-02-14 09:40:15 +0100

    play/stop audio

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,11 +6,14 @@ public class Rocket : MonoBehaviour
 {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>(); 
     }
 
     // Update is called once per frame
@@ -23,6 +26,16 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space)) {
             print("thrusting ");
             rigidBody.AddRelativeForce(Vector3.up);
+
+            if (!audioSource.isPlaying) {
+                audioSource.Play();
+            }
+
+             
+        } else if (Input.GetKeyUp(KeyCode.Space)) {
+            if (audioSource.isPlaying) {
+                audioSource.Stop();
+            }
         } 
         
         if (Input.GetKey(KeyCode.A)) {
